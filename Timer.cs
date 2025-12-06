56594bbea062aeaa5539e230df215b38b1a4ca9c
Roberto.coimbra <roberto_coimbra@estudante.sesisenai.org.br>
2025-12-05 21:46:13 -0300

    COUNTDOWN q reinicia o jogo
    Só criar um canva, dps um textMeshPro, e botar o script dentro do canva. Anexe o seu textMeshPro ao script

---
+++ Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour
+{
+    
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+
+
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (remainingTime < 0f)
+        {
+             SceneManager.LoadScene("SampleScene");
+
+        }
+        
+        
+    }
+}
